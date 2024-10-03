b8f19f480c7de52deb17a2a6b066b0d943d1aa4f
André Rocha <andrenakarocha@hotmail.com>
2024-10-02 22:31:39 -0300

    Feedbacks, indexação arrumada

--- src/components/Feedbacks/Feedbacks.jsx
+++ src/components/Feedbacks/Feedbacks.jsx
@@ -87,8 +87,6 @@ const SliderButton = styled.button`
 
 `;
 
-DescriptionRates
-
 const feedbackData = [
   { id: 1, avatar: 'https://images.unsplash.com/photo-1438761681033-6461ffad8d80?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', description: "Great product! Highly recommended.", rating: 5, name: 'Raquel' },
   { id: 2, avatar: 'https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?q=80&w=2080&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', description: "Good value for money. It does the job well.", rating: 4, name: 'Matheus' },
@@ -110,16 +108,16 @@ const FeedbackSlider = () => {
 
   return (
     <BenefitsContainer>
-    <BenefitsTitle>Feedbacks</BenefitsTitle>
-    <DescriptionRates>Veja alguns de nossos Feedbacks dos nossos clientes.</DescriptionRates>
-    <FeedbackContainer id="feedbacks">
-      <CardContainer translateX={-startIndex * 320}> 
-        {feedbackData.map(feedback => (
+      <BenefitsTitle>Feedbacks</BenefitsTitle>
+      <DescriptionRates>Veja alguns de nossos Feedbacks dos nossos clientes.</DescriptionRates>
+      <FeedbackContainer id="feedbacks">
+        <CardContainer translateX={-startIndex * 320}> 
+          {feedbackData.map(feedback => (
           <Card key={feedback.id}>
-                <FlexDiv>
-                    <Avatar src={feedback.avatar} alt="User avatar" />
-                    <p>{feedback.name}</p>
-                </FlexDiv>
+            <FlexDiv>
+              <Avatar src={feedback.avatar} alt="User avatar" />
+              <p>{feedback.name}</p>
+            </FlexDiv>
             <Description>{feedback.description}</Description>
             <StarRating>
               {[...Array(5)].map((_, index) => (
@@ -132,15 +130,15 @@ const FeedbackSlider = () => {
               ))}
             </StarRating>
           </Card>
-        ))}
-      </CardContainer>
-      <SliderButton direction="left" onClick={handlePrev} disabled={startIndex === 0}>
-        <ChevronLeft size={24} />
-      </SliderButton>
-      <SliderButton direction="right" onClick={handleNext} disabled={startIndex >= feedbackData.length - 3}>
-        <ChevronRight size={24} />
-      </SliderButton>
-    </FeedbackContainer>
+          ))}
+        </CardContainer>
+        <SliderButton direction="left" onClick={handlePrev} disabled={startIndex === 0}>
+          <ChevronLeft size={24} />
+        </SliderButton>
+        <SliderButton direction="right" onClick={handleNext} disabled={startIndex >= feedbackData.length - 3}>
+          <ChevronRight size={24} />
+        </SliderButton>
+      </FeedbackContainer>
     </BenefitsContainer>
   );
 };
